6c00998b40d810e4b890ce765da0c6046d1d5298
Achmad Fikri Fadhilah <achmadff12@gmail.com>
2023-01-17 16:16:31 +0700

    Fix

--- YukNgoding_Livecode/Program.cs
+++ YukNgoding_Livecode/Program.cs
@@ -1,31 +1,4 @@
-﻿// using YukNgoding_Livecode.Repository;
-// using YukNgoding_Livecode.Services;
-// using YukNgoding_Livecode.UI;
-//
-// class Program
-// {
-//     public static void Main(string[] args)
-//     {
-//         AppDbContext context = new AppDbContext();
-//         IPersistence persistence = new DbPersistence(context);
-//         ITraineeRepository traineeRepository = new TraineeRepository(context);
-//         ICourseRepository courseRepository = new CourseRepository(context);
-//         ICourseDetailRepository courseDetailRepository = new CourseDetailRepository(context);
-//         
-//         ITraineeService traineeService = new TraineeService(traineeRepository, persistence);
-//         ICourseService courseService = new CourseService(courseRepository, persistence);
-//         ICourseDetailService courseDetailService = new CourseDetailService(courseDetailRepository, persistence);
-//         
-//         TraineeView traineeView = new TraineeView(traineeService);
-//         CourseView courseView = new CourseView(courseService);
-//         TakeCourseView takeCourseView = new TakeCourseView(traineeService, courseService, courseDetailService);
-//         App app = new App(traineeView,courseView,takeCourseView);
-//         
-//         app.Run();
-//     }
-// }
-
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
